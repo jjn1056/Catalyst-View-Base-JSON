2824825d4eace60d874df3dfd4759af06092d825
John Napiorkowski <jjn1056@yahoo.com>
2016-12-23 09:01:15 -0600

    tweak readme

--- lib/Catalyst/View/Base/JSON.pm
+++ lib/Catalyst/View/Base/JSON.pm
@@ -134,8 +134,12 @@ sub TO_JSON { die "View ${\$_[0]->catalyst_component_name} must define a 'TO_JSO
 
 =head1 NAME
 
-
 Catalyst::View::Base::JSON - a 'base' JSON View 
+
+=for html
+<a href="https://badge.fury.io/pl/Catalyst-View-Base-JSON"><img src="https://badge.fury.io/pl/Catalyst-View-Base-JSON.svg" alt="CPAN version" height="18"></a>
+<a href="https://travis-ci.org/jjn1056/Catalyst-View-Base-JSON/"><img src="https://api.travis-ci.org/jjn1056/Catalyst-View-Base-JSON.png" alt="https://api.travis-ci.org/jjn1056/Catalyst-View-Base-JSON.png"></a>
+<a href="http://cpants.cpanauthors.org/dist/Catalyst-View-Base-JSON"><img src="http://cpants.cpanauthors.org/dist/Catalyst-View-Base-JSON".png" alt='Kwalitee Score' /></a>
 
 =head1 SYNOPSIS
 
